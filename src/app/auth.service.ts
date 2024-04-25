4c1cb695c1d16b9285f0b4c4445160a353bba442
freyaln <aurelien.freyling@gmail.com>
2024-04-25 21:06:49 +0200

    add

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,7 +10,7 @@ export interface Iuser {
   providedIn: 'root'
 })
 export class AuthService {
-  private url = 'https://nestjs-auth-w-guards-w-interceptors-w-decorators-w-relations.vercel.app';
+  private url = 'nestjs-auth-w-guards-w-interceptors-w-decorators-w-nv0m1g87u.vercel.app';
 
   constructor(private http: HttpClient) { }
 
